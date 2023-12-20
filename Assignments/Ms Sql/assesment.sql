bc977e5f0c7a27a961aa85234a2a567f1e9dcc28
Ganesh M <ganeshm08134@gmail.com>
2023-12-20 15:18:57 +0530

    second commit

---
+++ Assignments/Ms Sql/assesment.sql
@@ -0,0 +1,166 @@
+CREATE TABLE tblusers(
+	user_id INT PRIMARY KEY,
+	user_name VARCHAR(30),
+	Email VARCHAR(15)
+);
+
+
+CREATE TABLE tblcategory(
+	category_id INT PRIMARY KEY,
+	category_name VARCHAR(100)
+);
+
+ALTER TABLE tblcategory
+ADD COLUMN discription VARCHAR(100);
+
+CREATE TABLE tblProducts(
+	product_id INT PRIMARY KEY,
+	product_name VARCHAR(25),
+	quiantity INT,
+	product_price INT,
+	category_id INT REFERENCES tblcategory(category_id)
+);
+
+
+CREATE TABLE tblsales(
+	sales_id INT PRIMARY KEY,
+	sales_user_id INT REFERENCES tblusers(user_id),
+	Product_id INT REFERENCES tblProducts(product_id)
+);
+
+INSERT INTO tblusers VALUES(1001,'Akash','akash@gmail.com');
+INSERT INTO tblusers VALUES(1002,'Arvind','arvin@gmail.com');
+INSERT INTO tblusers VALUES(1003,'Sakshi','saksh@gmail.com');
+INSERT INTO tblusers VALUES(1004,'Kumar','kumar@gmail.com');
+
+
+INSERT INTO tblcategory VALUES(201,'Electronics','One stop for electronic items.')
+INSERT INTO tblcategory VALUES(202,'Apparel','Apparel is the next destination for fashion.')
+INSERT INTO tblcategory VALUES(203,'Grocery','All needs in one place.')
+
+
+INSERT INTO tblproducts values (1,'Mobile Phone',1000,15000,201)
+INSERT INTO tblproducts values (2,'Television',500,40000,201)
+INSERT INTO tblproducts values (3,'Denims',2000,700,202)
+INSERT INTO tblproducts values (4,'Vegetables',4000,40,203)
+INSERT INTO tblproducts values (5,'Ethnic Wear',300,1500,202)
+INSERT INTO tblproducts values (6,'Wireless Earphone',5000,2500,201)
+INSERT INTO tblproducts values (7,'Lounge Wear',200,1600,202)
+INSERT INTO tblproducts values (8,'Refrigerator',50,30000,201)
+INSERT INTO tblproducts values (9,'Pulses',60,150,202)
+INSERT INTO tblproducts values (10,'Fruits',100,250,202)
+
+SELECT * FROM tblproducts
+
+
+INSERT INTO tblsales VALUES(500,1001,1)
+INSERT INTO tblsales VALUES(501,1002,1)
+INSERT INTO tblsales VALUES(502,1003,2)
+INSERT INTO tblsales VALUES(504,1004,3)
+INSERT INTO tblsales VALUES(505,1004,1)
+INSERT INTO tblsales VALUES(506,1004,1)
+INSERT INTO tblsales VALUES(507,1002,2)
+INSERT INTO tblsales VALUES(508,1003,1)
+INSERT INTO tblsales VALUES(509,1001,7)
+INSERT INTO tblsales VALUES(510,1001,8)
+
+
+SELECT * FROM tblusers,tblcategory,tblproducts,tblsales
+
+
+
+-- 1.Write a function to fetch the names of the product,category and users along with the 
+-- cost for each product sold
+-- depending on the sales_id.
+-- Also if the cost for each product is more than 2000, then display a message stating
+-- that 'The product has gained profit'. 
+-- If the product cost is between 500 and 1000, then raise a message stating that
+-- 'The product has occured loss'. 
+-- If the product cost is less than 500, then raise an exception stating 'No profit no loss'.
+
+
+
+CREATE OR REPLACE FUNCTION UDF_asses(IN saleid INT)
+RETURNS REFCURSOR
+AS
+$$
+	DECLARE ref1 refcursor;
+	DECLARE prod_price INT; 
+	BEGIN
+		OPEN ref1 FOR SELECT product_name,category_name,user_name,product_price,(
+		CASE
+			WHEN (product_price > 2000) THEN 'The product has gained profit'
+			WHEN (product_price) BETWEEN 500 AND 1000 THEN 'The product has occured loss'
+		END
+		)
+		FROM tblusers tu
+		JOIN tblsales ts
+		ON ts.sales_user_id = tu.user_id
+		JOIN tblProducts tp
+		ON ts.product_id = tp.product_id
+		JOIN tblcategory tc
+		ON tp.category_id = tc.category_id
+		WHERE sales_id = saleid;
+
+		SELECT product_price INTO prod_price 
+		FROM tblProducts tp
+		JOIN tblsales ts
+		ON ts.Product_id = tp.product_id
+		WHERE sales_id = saleid;
+
+		IF (prod_price < 500) THEN
+			RAISE EXCEPTION'No profit no loss'
+		END IF
+		RETURN ref1;
+	END
+$$
+LANGUAGE PLPGSQL;
+
+
+BEGIN;
+SELECT UDF_asses(500);
+FETCH ALL IN "<unnamed portal 4>";
+COMMIT;
+
+SELECT * FROM tblProducts
+
+
+-- 2.Write a procedure to update the name of the category from 'Electronics' to 'Modern Gadgets'
+-- and also 
+-- fetch the category and product names when the userid is passed as the input parameter.
+
+
+
+CREATE OR REPLACE PROCEDURE USP_upFETCH(IN usr_id INT,OUT Ret REFCURSOR)
+LANGUAGE PLPGSQL
+AS
+$$
+	BEGIN
+		UPDATE tblcategory
+		SET category_name = 'Modern Gadgets'
+		WHERE category_name = 'Electronics';
+
+		IF((usr_id IS NULL) OR (EXISTS (SELECT user_id FROM tblusers WHERE user_id = usr_id))) THEN
+
+			OPEN Ret FOR SELECT category_name,product_name
+			FROM tblusers tu
+			JOIN tblsales ts
+			ON ts.sales_user_id = tu.user_id
+			JOIN tblProducts tp
+			ON ts.product_id = tp.product_id
+			JOIN tblcategory tc
+			ON tp.category_id = tc.category_id
+			WHERE user_id = usr_id;
+		ELSE 
+			RAISE EXCEPTION 'USER NOT FOUND';
+		END IF;
+
+	END;
+$$
+
+BEGIN;
+CALL USP_upFETCH(1001,NULL);
+FETCH ALL IN "<unnamed portal 5>";
+COMMIT;
+
+SELECT * FROM tblcategory;
